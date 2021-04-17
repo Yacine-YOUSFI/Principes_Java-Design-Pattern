52f46499fad1efb1dce663dadb6a7384f976ab96
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-18 01:35:09 +0200

    La serialisation de la classe groupe personnel dans le fichier pers_grp.ser et la classe personnel dans pers.ser

--- src/main/java/fr/uvsq/poo/Builder_Composite_Iterator/Serialiser.java
+++ src/main/java/fr/uvsq/poo/Builder_Composite_Iterator/Serialiser.java
@@ -10,10 +10,20 @@ import java.time.format.DateTimeFormatter;
 public class Serialiser {
 
 
+
+
     public static void main (String[] args) throws FileNotFoundException {
-        Personnel ph ;
+        Personnel ph ,pg,px;
+        Personnel_groupe pgg;
+        pg = new Personnel.Builder("J", "Jacques", "Plombier").build();
+        px = new Personnel.Builder("Jj", "Jacques", "Plombier").build();
+
+        pgg= new Personnel_groupe();
+        pgg.addPersonnel(pg);
+        pgg.addPersonnel(px);
 
 
+        //Serialiser Personnel/////////////////////////////
         ph = new Personnel.Builder("Jean", "Jacques", "Plombier")
                 .dateNaissance(LocalDate.parse("1970-01-01", DateTimeFormatter.ISO_DATE)).build();
         System.out.println(ph);
@@ -32,9 +42,29 @@ public class Serialiser {
             e.printStackTrace();
         }
 
+        //Serialiser Groupe_Personnel/////////////////
+
+        try {
+            FileOutputStream fo= new FileOutputStream("pers_grp.ser");
+            ObjectOutputStream ops= new ObjectOutputStream(fo);
+
+            ops.writeObject(pgg);
+
+            ops.close();
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
+
+
+
+}
 
 
 
