31f7022fa954aadd69906248f5e0caec96d49c14
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-18 23:05:22 +0200

    Implementation des tests unitaires pour les operation CRUD

--- src/test/java/fr/uvsq/poo/Builder/Serialiser_test.java
+++ src/test/java/fr/uvsq/poo/Builder/Serialiser_test.java
@@ -1,8 +1,6 @@
 package fr.uvsq.poo.Builder;
 
-import fr.uvsq.poo.Builder_Composite_Iterator.NumeroTelephone;
-import fr.uvsq.poo.Builder_Composite_Iterator.Personnel;
-import fr.uvsq.poo.Builder_Composite_Iterator.Personnel_groupe;
+import fr.uvsq.poo.Builder_Composite_Iterator.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -73,6 +71,25 @@ public class Serialiser_test {
             e.printStackTrace();
         }
         assertEquals(pg, qg);
+    }
+
+    @Test()
+    public void testPersonnelCRUD() {
+        InterfaceDAO<Personnel> pc = DAOFactory.getPersonneldao();
+        pc.delete(p); 
+        pc.create(p);
+        pc.update(p);
+        assertNotEquals(p, pc.read(p.getNom()));
+    }
+
+    @Test()
+    public void testPersonnelGroupeCRUD() {
+        InterfaceDAO<Personnel_groupe> pgc = DAOFactory.getPersonnelgrpdao();
+        pgc.delete(pg);
+        pgc.create(pg);
+        pg.addPersonnel(p);
+        pgc.update(pg);
+        assertNotEquals(pg, pgc.read(pg.getId()));
     }
 
 
