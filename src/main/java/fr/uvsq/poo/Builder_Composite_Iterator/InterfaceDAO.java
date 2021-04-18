fb5b7090397fa75b5c7c3884329dfead00c2a66d
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-18 21:26:50 +0200

    Implemntation de l'interface DAO qui permet de définir les methodes CRUD

---
+++ src/main/java/fr/uvsq/poo/Builder_Composite_Iterator/InterfaceDAO.java
@@ -0,0 +1,17 @@
+package fr.uvsq.poo.Builder_Composite_Iterator;
+
+public interface InterfaceDAO <T>{
+    /**
+     * Les methodes CRUD
+     * @param objet
+     * @return
+     * //
+     */
+    T create(T objet);
+    T read(String id);
+    T update(T objet);
+    void delete(T objet);
+
+
+
+}
